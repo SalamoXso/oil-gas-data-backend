4d497071ef0caaf2656fc822cbf468b5011102c1
SalamoXso <111354463+SalamoXso@users.noreply.github.com>
2025-01-22 14:05:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-# build.sh
+# Install system dependencies
+sudo apt-get update
+sudo apt-get install -y \
+    libgstreamer1.0-0 \
+    libgstreamer-plugins-base1.0-0 \
+    libgstgl-1.0-0 \
+    libgstcodecparsers-1.0-0 \
+    libavif15 \
+    libenchant-2-2 \
+    libsecret-1-0 \
+    libmanette-0.2-0 \
+    libgles2
 
 # Install Python dependencies
 pip install -r requirements.txt
